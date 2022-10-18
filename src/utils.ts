0fef254b0b83418421f1d1dfa1d008bbb2225730
Surdu <neon-doubt@maskmail.net>
2022-10-19 00:34:43 +0300

    Implemented developersText function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import authors from "../AUTHORS.md";
+
 export function random(min: number, max: number) {
 	return Math.random() * (max - min) + min;
 }
@@ -11,4 +13,33 @@ export function timeText() {
 	return new Intl.DateTimeFormat("default", { timeStyle: "medium" }).format(
 		date
 	);
+}
+
+let authorsIndex = 0;
+let prevSecond = -1;
+
+export function developersText() {
+	const contributors = authors
+		.split("\n")
+		.filter((line) => line[0] === "-")
+		.map((line) => {
+			const regex = /-.\[(.*?)\]/;
+			const match = regex.exec(line);
+
+			if (!match) {
+				return;
+			}
+
+			return match[1];
+		});
+
+	const value = contributors[authorsIndex % contributors.length];
+
+	const second = new Date().getSeconds();
+	if (second % 10 === 0 && prevSecond !== second) {
+		prevSecond = second;
+		authorsIndex += 1;
+	}
+
+	return value;
 }
