fbb406d785e602889040b32948049360de7339f2
Surdu <neon-doubt@maskmail.net>
2022-10-19 22:55:23 +0300

    Added volcanos text

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,44 @@
 import authors from "../AUTHORS.md";
 
+const contributors = authors
+	.split("\n")
+	.filter((line) => line[0] === "-")
+	.map((line) => {
+		const regex = /-.\[(.*?)\]/;
+		const match = regex.exec(line);
+
+		if (!match) {
+			return "";
+		}
+
+		return match[1];
+	});
+shuffle(contributors);
+
+const volcanos = [
+	"Hood",
+	"Jefferson",
+	"Bachelor",
+	"Lassen",
+	"Broken Top",
+	"St. Helens",
+	"Three Fingered Jack",
+	"Shasta",
+	"Thielson",
+	"Baker",
+	"Adams",
+	"Rainier",
+	"Garibaldi",
+	"Washington",
+	"Three Sisters",
+	"Glacier",
+];
+shuffle(volcanos);
+
+function shuffle(array: string[]) {
+	return array.sort(() => 0.5 - Math.random());
+}
+
 export function random(min: number, max: number) {
 	return Math.random() * (max - min) + min;
 }
@@ -15,31 +54,25 @@ export function timeText() {
 	);
 }
 
-let authorsIndex = 0;
+let rollingIndex = 0;
 let prevSecond = -1;
 
-export function developersText() {
-	const contributors = authors
-		.split("\n")
-		.filter((line) => line[0] === "-")
-		.map((line) => {
-			const regex = /-.\[(.*?)\]/;
-			const match = regex.exec(line);
-
-			if (!match) {
-				return;
-			}
-
-			return match[1];
-		});
-
-	const value = contributors[authorsIndex % contributors.length];
+function getRollingText(collection: string[]) {
+	const value = collection[rollingIndex % collection.length];
 
 	const second = new Date().getSeconds();
 	if (second % 10 === 0 && prevSecond !== second) {
 		prevSecond = second;
-		authorsIndex += 1;
+		rollingIndex += 1;
 	}
 
 	return value;
+}
+
+export function developersText() {
+	return getRollingText(contributors);
+}
+
+export function volcanoText() {
+	return getRollingText(volcanos);
 }
